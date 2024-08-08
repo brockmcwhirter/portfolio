873ad48d9e2e854acf53b26810b0833ce871eb15
brockmcwhirter <43620656+brockmcwhirter@users.noreply.github.com>
2024-08-08 14:21:13 -0500

    home section final

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,14 @@ const Home: React.FC = () => {
           <p className="mt-4 text-lg">
             My name is Macky Brock McWhirter. I am a passionate technologist with a solid foundation in software engineering. I specialize in creating sophisticated software applications. I have a strong proficiency in machine learning, allowing me to construct and deploy complex models for various applications. Currently, I am pursuing a Master's degree in Electrical & Computer Engineering at Johns Hopkins University. My studies focus on an integrative approach that combines software engineering, machine learning, and Digital Signal Processing (DSP). This unique blend of disciplines enables me to merge theoretical insights with practical applications, preparing me for the challenges of these rapidly evolving fields. Welcome to my portfolio, where you can explore my work experience, educational background, technical skills, and projects that I have developed.
           </p>
-          <p className="mt-4 text-lg">
-            Just want to download a resume? <a href="/MackyMcWhirterResume.pdf" download className="text-blue-500 hover:underline">Click here</a>
-          </p>
+          <div className="mt-8">
+            <p className="text-lg">
+              Prefer to see all my qualifications at a glance? Download my resume below.
+            </p>
+            <a href="MackyMcWhirterResume.pdf" download className="inline-block bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300 mt-4">
+              Download My Resume
+            </a>
+          </div>
         </section>
         <section id="experience" className="min-h-screen bg-gray-200 p-8 pt-24">
           <h1 className="text-4xl font-bold">Experience</h1>
