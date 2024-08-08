8998567158c595e1cf72aff1d40ba13a5fa8421d
brockmcwhirter <43620656+brockmcwhirter@users.noreply.github.com>
2024-08-08 14:13:59 -0500

    Resume addition

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,14 @@ const Home: React.FC = () => {
     <div>
       <Navbar />
       <Sidebar />
-      <div className="pl-64">
+      <div className="pl-64 ">
         <section id="home" className="min-h-screen bg-gray-100 p-8 pt-24">
-          <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
+          <h1 className="text-4xl font-bold">Welcome!</h1>
           <p className="mt-4 text-lg">
-            Hi, I'm [Your Name], a [Your Profession]. I have a passion for [Your Interests/Profession]. Welcome to my portfolio where you can learn more about my work and skills.
+            My name is Macky Brock McWhirter. I am a passionate technologist with a solid foundation in software engineering. I specialize in creating sophisticated software applications. I have a strong proficiency in machine learning, allowing me to construct and deploy complex models for various applications. Currently, I am pursuing a Master's degree in Electrical & Computer Engineering at Johns Hopkins University. My studies focus on an integrative approach that combines software engineering, machine learning, and Digital Signal Processing (DSP). This unique blend of disciplines enables me to merge theoretical insights with practical applications, preparing me for the challenges of these rapidly evolving fields. Welcome to my portfolio, where you can explore my work experience, educational background, technical skills, and projects that I have developed.
+          </p>
+          <p className="mt-4 text-lg">
+            Just want to download a resume? <a href="/MackyMcWhirterResume.pdf" download className="text-blue-500 hover:underline">Click here</a>
           </p>
         </section>
         <section id="experience" className="min-h-screen bg-gray-200 p-8 pt-24">
