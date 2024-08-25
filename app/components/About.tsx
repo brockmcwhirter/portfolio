5c3103b4eb1178426404e1c486e10b8f56b977c9
brockmcwhirter <43620656+brockmcwhirter@users.noreply.github.com>
2024-08-25 00:35:39 -0500

    damn it

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -14,30 +14,34 @@ const About: React.FC<AboutProps> = ({ darkMode }) => {
   return (
     <section
       id="about"
-      className={`min-h-screen p-6 sm:p-8 pt-24 scroll-mt-3 ${darkMode ? 'bg-dark-background text-light-text' : 'bg-primary text-text'}`}
+      className={`min-h-screen bg-primary dark:bg-dark-background text-text dark:text-light-text p-8 pt-24 scroll-mt-3`}
     >
-      <div className="max-w-6xl mx-auto">
-        <h2 className="text-3xl sm:text-4xl font-bold">Welcome!</h2>
-        <div className="mt-8 space-y-4">
-          {content.map((paragraph, index) => (
-            <p key={index} className="text-base sm:text-lg indent-4 sm:indent-8">
-              {paragraph}
-            </p>
-          ))}
-        </div>
-        <div className="mt-8">
-          <p className="text-base sm:text-lg">
-            Interested in a quick overview? View my resume below.
-          </p>
-          <a
-            href="https://drive.google.com/drive/folders/1I6pycIFp26E3hloy86wPBva1RvFjot1_?usp=sharing"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={`inline-block font-bold py-2 px-4 rounded transition duration-300 mt-4 ${darkMode ? 'bg-dark-buttonBg text-dark-buttonText hover:bg-dark-accent' : 'bg-black text-white hover:bg-gray-800'}`}
-            aria-label="View my resume on Google Drive in a new tab"
-          >
-            View My Resume
-          </a>
+      <div className="space-y-16">
+        <div className="flex flex-col sm:flex-row items-start justify-between">
+          <div className="sm:w-3/4 max-w-full">
+            <h2 className="text-4xl font-bold">Welcome!</h2>
+            <div className="mt-8 space-y-4">
+              {content.map((paragraph, index) => (
+                <p key={index} className="text-base sm:text-lg indent-4 sm:indent-8">
+                  {paragraph}
+                </p>
+              ))}
+            </div>
+            <div className="mt-8">
+              <p className="text-base sm:text-lg">
+                Interested in a quick overview? View my resume below.
+              </p>
+              <a
+                href="https://drive.google.com/drive/folders/1I6pycIFp26E3hloy86wPBva1RvFjot1_?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`inline-block font-bold py-2 px-4 rounded transition duration-300 mt-4 ${darkMode ? 'bg-dark-buttonBg text-dark-buttonText hover:bg-dark-accent' : 'bg-black text-white hover:bg-gray-800'}`}
+                aria-label="View my resume on Google Drive in a new tab"
+              >
+                View My Resume
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </section>
