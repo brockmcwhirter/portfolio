20b17680c56cc2eb0fdddea59f1cf91ccbf93dd5
brockmcwhirter <43620656+brockmcwhirter@users.noreply.github.com>
2024-08-25 00:17:42 -0500

    layout adjustment for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,15 +19,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Add a viewport meta tag for responsive design */}
+        {/* Script to set theme before hydration */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                let theme = localStorage.getItem('theme') || 'system';
+                if (theme === 'system') {
+                  const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+                  theme = darkModeMediaQuery.matches ? 'dark' : 'light';
+                }
+                document.documentElement.classList.add(theme);
+              })();
+            `,
+          }}
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={`${inter.className} bg-primary dark:bg-dark-background text-text dark:text-light-text`}>
-        <ThemeProvider attribute="class" enableSystem={true} defaultTheme="system">
-          <ClientWrapper>
+        <ClientWrapper>
+          <ThemeProvider attribute="class" enableSystem={true} defaultTheme="system">
             {children}
-          </ClientWrapper>
-        </ThemeProvider>
+          </ThemeProvider>
+        </ClientWrapper>
       </body>
     </html>
   );
